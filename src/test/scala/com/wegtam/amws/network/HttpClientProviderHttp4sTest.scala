84f2ca105be7b18e0240f1741dbdaf91f8b817f2
Maalikamalini Chandrasekar <maali@wegtam.com>
2020-01-13 13:30:33 +0100

    amws-scala-2
    Client provider Http4sTest

--- src/test/scala/com/wegtam/amws/network/HttpClientProviderHttp4sTest.scala
+++ src/test/scala/com/wegtam/amws/network/HttpClientProviderHttp4sTest.scala
@@ -9,18 +9,11 @@
 package com.wegtam.amws.network
 
 import java.net.{ ServerSocket, URI }
-
+import java.util.concurrent.Executors
 import cats.effect._
-import org.http4s._
-import org.http4s.client._
-import org.http4s.client.dsl.io._
-import org.http4s.dsl.io._
-import org.http4s.server.blaze._
-import akka.http.scaladsl.server.{ Directives, Route }
 import org.scalatest.{ AsyncWordSpec, BeforeAndAfterAll, MustMatchers }
-
-import scala.concurrent.Await
-import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+import org.http4s.client._
 
 class HttpClientProviderHttp4sTest extends AsyncWordSpec with MustMatchers with BeforeAndAfterAll {
 
@@ -31,7 +24,7 @@ class HttpClientProviderHttp4sTest extends AsyncWordSpec with MustMatchers with 
     * @param reuseAddress If set to `false` the returned port will not be useable for some time.
     * @return A port number.
     */
-  private def findAvailablePort(reuseAddress: Boolean): Int = {
+  protected def findAvailablePort(reuseAddress: Boolean): Int = {
     val serverSocket = new ServerSocket(0)
     val freePort     = serverSocket.getLocalPort
     serverSocket.setReuseAddress(reuseAddress)
@@ -39,4 +32,63 @@ class HttpClientProviderHttp4sTest extends AsyncWordSpec with MustMatchers with 
     freePort
   }
 
+  "HttpClientProvider" when {
+    "using Http4s" when {
+      "using get" when {
+        "endpoint is not found" must {
+          "return the correct response data" in {
+            val port = findAvailablePort(true)
+//            val routes = HttpRoutes.of[IO] {
+//              case r => Ok(r.headers.get(Host).map(_.value).getOrElse("None"))
+//            }
+//            val route: HttpRoutes[IO] = Router{
+//            case GET =>
+//            }
+//
+//            val _ = Http().andThen(route, "localhost", port)
+            val uri = new URI(s"http://localhost:$port/get-me-wrong")
+            val pld = AmwsRequestPayload(data = None)
+
+            val blockingEC   = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(5))
+            implicit val bar = scala.concurrent.ExecutionContext.Implicits.global
+            implicit val foo = cats.effect.IO.contextShift(bar)
+            implicit val httpClient: Client[IO] =
+              JavaNetClientBuilder[IO](blockingEC).create
+
+            val client = new HttpClientProviderHttp4s()
+            val a      = client.get(uri)(pld)
+            println(a)
+            val b = a.unsafeRunSync()
+            println(b.body)
+            b.body must be("foo")
+          }
+        }
+
+
+
+//        "endpoint is valid" must {
+//          "return the correct response data" in {
+//            val port  =findAvailablePort(true)
+//            val expectedResponse = "I am the reponse!"
+//            val route: HttpRoutes[IO] = Router{
+//              case GET -> Root / "get-me" => "you should reach me"
+//            }
+//
+//            val _ = Http().andThen(route, "localhost", port)
+//
+//            val uri = new URI(s"http://localhost:$port/get-me")
+//            val pld = AmwsRequestPayload(data = None)
+//
+//            val client = new HttpClientProviderHttp4s()
+//            client.get(uri)(pld).map{
+//              case Left(e) => fail(s"Http request returned an error: $e")
+//              case Right(d) => d.b
+//            }
+//
+//          }
+//        }
+
+      }
+    }
+  }
 }
